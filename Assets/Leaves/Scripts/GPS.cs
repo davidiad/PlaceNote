a47982423e22388d9fc707b99f76d2c44a36ad8b
David Fierstein <studio@davidiad.com>
2018-05-30 21:23:15 -0700

    Add GPS location functionality

---
+++ Assets/Leaves/Scripts/GPS.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPS : MonoBehaviour {
+
+    public static GPS Instance { set; get;  }
+
+    public float longtitude;
+    public float latitude;
+
+	void Start () {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        StartCoroutine(StartLocationService());
+	}
+
+    private IEnumerator StartLocationService()
+    {
+        if (!Input.location.isEnabledByUser) {
+            Debug.Log("GPS not enabled by User");
+            yield break;
+        }
+
+        Input.location.Start();
+        int maxWait = 20;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0) {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+        if (maxWait <= 0)
+        {
+            Debug.Log("Timed Out");
+            yield break;
+        }
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            Debug.Log("Could not determine location");
+            yield break;
+        }
+
+        longtitude = Input.location.lastData.longitude;
+        latitude = Input.location.lastData.latitude;
+
+        yield break;
+    }
+
+    void Update () {
+		
+	}
+}
---
+++ Assets/Leaves/Scripts/UpdateGPSText.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateGPSText : MonoBehaviour {
+
+    public Text coordinates;
+	
+	void Update () {
+        coordinates.text = "Lon: " + GPS.Instance.longtitude.ToString("F7") +
+                           ", Lat: " + GPS.Instance.latitude.ToString("F7");
+        Debug.Log(coordinates.text);
+	}
+}
